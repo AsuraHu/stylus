63d09f12263be56cc941d5e67ef4860b9f57e10f
AsuraHu <1030320304@qq.com>
2019-11-15 17:09:42 +0800

    df

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <p>圣诞节sad九分裤阿凡达萨迪克减肥放假奥斯卡暗示领导看风景氨基酸的疯狂灵活</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style scoped lang='stylus'>
+  @import '~style/mixins.styl'
+  @import '~style/varibles.styl'
+  div
+    // overflow: hidden
+    // min-width: 0px
+    color: red
+    // display: flex
+    p
+      // flex: 1
+      ellipsis()
+</style>
